dfbb7b17206026b96bb84e4acbe11324e9fea30a
Nikolay Russkin <nrusskin@gmail.com>
2015-09-16 16:15:51 +0300

    added 'out of range' assertions Bitset indexing fixes

--- include/bitset.h
+++ include/bitset.h
@@ -1,6 +1,8 @@
-#ifndef __BITSET_H__
+#ifndef __LIBBITSET_BITSET_H__
+#define __LIBBITSET_BITSET_H__
 
 #include <cstring> //memset
+#include <cassert>
 #include "iterator.h"
 #include "streamreader.h"
 
@@ -9,14 +11,14 @@ namespace libbitset {
 typedef unsigned long long size_t;
 typedef unsigned long long uint64_t;
 
-template <size_t size_,
-	typename BaseType = unsigned char>
+template <size_t size_, typename BaseType = unsigned char>
 class Bitset{
 	public:
 		typedef libbitset::size_t size_t;
 		typedef BaseType value_type;
 		typedef Iterator<Bitset> iterator;
 
+		// cached values
 		const size_t value_type_size = sizeof(value_type) << 3;
 		const uint64_t value_type_mask = sizeof(value_type)==sizeof(uint64_t) ? (uint64_t)-1 : ((uint64_t)1 << value_type_size) - 1;
 		const size_t array_length = size_ / value_type_size + ((size_ & (value_type_size - 1)) ? 1 : 0);
@@ -26,14 +28,12 @@ class Bitset{
 			clear_();
 		}
 	
+		// TODO move to BitsetFactory
 		template <typename It>
-		Bitset(const It& begin, const It& end) {
+		Bitset(It begin, It end) {
 			StreamRedader reader;
 			clear_();
-			while(begin != end) {
-				reader(*this, *begin);
-				++begin;
-			}
+			reader(begin, end, *this);
 		}
 
 		Bitset(uint64_t rhs) {
@@ -52,12 +52,41 @@ class Bitset{
 		~Bitset(){}
 
 		size_t size() const {return size_;}
+
+		// iterator access
 		iterator operator [] (size_t idx) {
-			return iterator(&value_[idx >> ( sizeof(value_type) + 2)], idx & (value_type_size- 1));
+			return iterator(&value_[idx >> ( sizeof(value_type) + 2)], 
+				idx & (value_type_size - 1));
 		}
 		iterator begin() {return (*this)[0];}
 		iterator end() {return (*this)[size_];}
+		// --iterator access
 
+		// direct access
+		void set(size_t idx) {
+			assert(idx < size_);
+			value_[idx >> (sizeof(value_type) + 2)] |= 1 << (idx & (value_type_size - 1) );
+		}
+
+		void clear(size_t idx) {
+			assert(idx < size_);
+			value_[idx >> value_type_size] &= ~(1 << (idx & value_type_mask));
+		}
+
+		bool test(size_t idx) const {
+			assert(idx < size_);
+			return value_[idx >> (sizeof(value_type) + 2)] & (1 << (idx & (value_type_size - 1)));
+		}
+		// --direct access
+
+		// read stream of sizeof(value_type) bytes
+		size_t on_read_data(value_type val, size_t idx) {
+			// TODO assert( idx < array_length
+			value_[idx] = val;
+			return ++idx;
+		}
+
+		// return the first sizeof(uint_64) bytes
 		uint64_t value() const {
 			// TODO: assert(array_size <= sizeof(uint64_t)
 			if (sizeof(value_type) == sizeof(uint64_t))	//TODO: don't use base type uint64_t
@@ -68,28 +97,6 @@ class Bitset{
 				res = (res << value_type_size) | value_[--idx];
 			return res;
 		}
-
-		void set(size_t idx) {
-			// value[idx/sizeof(BaseType)/8][idx%(sizeof(BaseType)*8)] = 1
-			value_[idx >> (sizeof(value_type) + 2)] |= 1 << (idx & (value_type_size - 1) );
-		}
-
-		void clear(size_t idx) {
-			value_[idx >> value_type_size] &= ~(1 << (idx & value_type_mask));
-		}
-
-		bool test(size_t idx) const {
-			// TODO assert( base_type_size*len(value_) > idx);
-			return value_[idx >> value_type_size] & (1 << (idx & value_type_mask));
-		}
-
-		// read stream of sizeof(value_type) bytes
-		size_t on_read_data(value_type val, size_t idx) {
-			// TODO assert( idx < array_length
-			value_[idx] = val;
-			return ++idx;
-		}
-
 	private:
 		void clear_() {
 			memset(value_, 0, sizeof(value_));
@@ -97,7 +104,7 @@ class Bitset{
 		//Storage new_value_;
 		value_type value_[(size_ >> (sizeof(value_type) + 2)) + 
 			((size_ & ((sizeof(value_type) << 3) - 1)) ? 1 : 0)];
-};
+};// Bitset
 
-};//namespace
-#endif//__BITSET_H__
+};// namespace
+#endif// __BITSET_H__
--- test/unit/streamreader.cpp
+++ test/unit/streamreader.cpp
@@ -12,17 +12,30 @@ struct StreamReaderTestStorage {
 	value_type data[4];
 };
 
-TEST(streamreader_test, testname) {
+class StreamReaderTest : public ::testing::Test {
+public:
 	StreamRedader reader;
-	StreamReaderTestStorage storage;
 	StreamReaderTestStorage::value_type test_data[4] = {3,4,5,6}; 
 	unsigned char size = sizeof(test_data)/sizeof(test_data[0]);
 	StreamReaderTestStorage::value_type *begin = &test_data[0], *end = &test_data[size];
+	
+};
 
+TEST_F(StreamReaderTest, test1) {
+	StreamReaderTestStorage storage;
 	while (begin != end) {
 		reader(storage, *begin);
 		++begin;
 	}
+
+	for (unsigned char idx = 0; idx < size; ++idx) {
+    ASSERT_EQ(test_data[idx], storage.data[idx])  << "Vectors differ at index " << idx;
+	}
+}
+
+TEST_F(StreamReaderTest, test2) {
+	StreamReaderTestStorage storage;
+	reader(begin, end, storage);
 
 	for (unsigned char idx = 0; idx < size; ++idx) {
     ASSERT_EQ(test_data[idx], storage.data[idx])  << "Vectors differ at index " << idx;
